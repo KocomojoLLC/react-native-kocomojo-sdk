98fb6f7603c7f92d835a0975bd065e052036b9a4
Elijah Windsor <ewindsor@gmail.com>
2017-08-01 13:35:14 -0400

    Added linkDependencies.sh

---
+++ linkDependencies.sh
@@ -0,0 +1,8 @@
+react-native link react-native-bluetooth-state
+react-native link react-native-device-info
+react-native link react-native-geocoder
+react-native link react-native-ibeacon
+react-native link react-native-location
+react-native link react-native-video
+react-native link tipsi-stripe
+react-native link react-native-fbsdk
